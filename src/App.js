84ea87b4799d180bd47eae24d872d53d73a8184d
Vaishnavi Rama_Shanmugam <vaishnavi@hashdhronas.com>
2022-01-08 15:29:18 +0530

    Initial commit with mock data

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+import Route from "./components/route";
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Route />
+      </>
+    );
+  }
+}
+
+export default App;
